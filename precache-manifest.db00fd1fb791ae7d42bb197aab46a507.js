91a96736d18c99e57fcc88b700688e22a366e0e5
fanbyprinciple <fanbyprinciple@gmail.com>
2021-04-23 17:07:11 +0530

    Updates

---
+++ precache-manifest.db00fd1fb791ae7d42bb197aab46a507.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b296c6aac2a8a2accd9",
+    "url": "/me/static/css/main.d0f97265.chunk.css"
+  },
+  {
+    "revision": "8b296c6aac2a8a2accd9",
+    "url": "/me/static/js/main.8b296c6a.chunk.js"
+  },
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "6a980dc900b35dd4de8489c5498bd29c",
+    "url": "/me/index.html"
+  }
+];
